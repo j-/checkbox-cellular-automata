6072c900642cbcb607d5cc3f915f1caa2fb404dd
j- <j@skeoh.com>
2020-08-08 20:34:22 +1000

    Updates

---
+++ precache-manifest.af8debdba88b880e3f0670f966cdecea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3438db7374528f3c20d92efab3f3240b",
+    "url": "/checkbox-cellular-automata/index.html"
+  },
+  {
+    "revision": "1bf597b81f3b0850a6a9",
+    "url": "/checkbox-cellular-automata/static/css/2.dfbc895d.chunk.css"
+  },
+  {
+    "revision": "7ae10c5558f9482d12bd",
+    "url": "/checkbox-cellular-automata/static/css/main.13331880.chunk.css"
+  },
+  {
+    "revision": "1bf597b81f3b0850a6a9",
+    "url": "/checkbox-cellular-automata/static/js/2.80eda5f1.chunk.js"
+  },
+  {
+    "revision": "1c9a5c14c094f70c35a5a57a5770d494",
+    "url": "/checkbox-cellular-automata/static/js/2.80eda5f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae10c5558f9482d12bd",
+    "url": "/checkbox-cellular-automata/static/js/main.4bc6d200.chunk.js"
+  },
+  {
+    "revision": "3b93d8447207c4ea6ca8",
+    "url": "/checkbox-cellular-automata/static/js/runtime-main.7f43383b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/checkbox-cellular-automata/precache-manifest.af8debdba88b880e3f0670f966cdecea.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/checkbox-cellular-automata/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
